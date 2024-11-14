20b60d354ac2eeee3a7a9b84f91cff259f99c71d
linhqngo <124748091+linhqngo@users.noreply.github.com>
2024-11-14 00:47:18 -0500

    Add files via upload
    Updated files

---
+++ urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from authapp import views
+
+urlpatterns = [
+    path('', views.Home, name = "Home"),
+    path('signup', views.signup,name="signup"),
+    path('login', views.handlelogin,name="handlelogin"),
+    path('logout', views.handleLogout,name="handlelogout"),
+    
+]
